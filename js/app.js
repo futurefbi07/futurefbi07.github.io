f63110bfa5096282f93d40e5968175c8dcabf145
Lakisha Davis <lakisha1.davis@gmail.com>
2018-07-27 21:24:47 -0400

    Edits to JS files

--- js/app.js
+++ js/app.js
@@ -2,69 +2,68 @@
 of Matthew Cranford's Arcade Game Walkthrough, see
 https://matthewcranford.com/arcade-game-walkthrough-part-1-starter
 -code-breakdown/*/
-
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
-  // Variables applied to each of our instances go here,
-  // we've provided one for you to get started
-  this.x = x;
-  this.y = y + 55;
-  this.step = 101;
-  this.boundary = this.step * 5;
-  //Set speed of the enemy
-  this.speed = speed;
-  // The image/sprite for our enemies, this uses
-  // a helper we've provided to easily load images
-  this.sprite = 'images/enemy-bug.png';
+var Enemy = function(x,y, speed) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+    this.x = x;
+    this.y = y + 55;
+    this.step = 101;
+    this.boundary = this.step * 5;
+    //Set speed of the enemy
+    this.speed = speed;
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
 
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-  if (this.x < this.boundary) {
-    // move forward
-    // calcularte speed of the enemy
-    // by multiplying any movement by the dt parameter
-    // which will give the enemy a constant speed across the gameboar
-    this.x = this.x + this.speed * dt;
-  } else {
-    // move to start position
-    this.x = -1;
-  }
+    if(this.x < this.boundary){
+      // move forward
+      // calcularte speed of the enemy
+      // by multiplying any movement by the dt parameter
+      // which will give the enemy a constant speed across the gameboar
+      this.x = this.x + this.speed * dt;
+    }
+    else {
+      // move to start position
+      this.x = -1;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 class Player {
-  constructor() {
+  constructor(){
     this.x = 202;
     this.y = 387;
     this.finish = false
     this.sprite = 'images/char-boy.png';
-  }
+}
 
   update() {
     //Did the player get hit
-    for (let enemy of allEnemies) {
+      for(let enemy of allEnemies) {
 
-      if (this.y === enemy.y && (enemy.x + enemy.step / 2 > this.x && enemy.x < this.x + 202 / 2)) {
-        this.reset();
-      }
+        if(this.y === enemy.y && (enemy.x + enemy.step/2 > this.x && enemy.x < this.x + 202/2)) {
+          this.reset();
+        }
 
-      //Did the player reach the last tile
-      if (this.y === -28) {
-        this.finish = true;
+        //Did the player reach the last tile
+        if(this.y === -28){
+          this.finish = true;
+        }
       }
-    }
   }
-  // Reset the player's position to the start location
   reset() {
     this.x = 202;
     this.y = 387;
@@ -72,38 +71,42 @@ class Player {
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
+// Update the players's position, required method for game
 
+// Parameter: dt, a time delta between ticks
 
-  // Direction of travel according to player input
-  handleInput(input) {
-    switch (input) {
+/*@param {string} input -- Direction to Travel According to Input*/
+
+handleInput(input){
+  switch(input){
       case 'left':
-        if (this.x > 0) {
+        if(this.x > 0){
           this.x = this.x - 101;
         }
-        break;
+          break;
       case 'right':
-        if (this.x < 400) {
+        if(this.x < 400){
           this.x = this.x + 101;
         }
-        break;
+          break;
       case 'up':
-        if (this.y > 0) {
+        if(this.y > 0){
           this.y = this.y - 83;
         }
-        break;
+          break;
       case 'down':
-        if (this.y < 332) {
+        if(this.y < 332){
           this.y = this.y + 83;
         }
-        break;
-    }
+          break;
   }
+}
 }
 
 // Now instantiate your objects.
+
 // Place all enemy objects in an array called allEnemies
-const allEnemies = [];
+const allEnemies =[];
 
 // Place the enemy object in a variable
 
@@ -119,12 +122,12 @@ const player = new Player();
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-  var allowedKeys = {
-    37: 'left',
-    38: 'up',
-    39: 'right',
-    40: 'down'
-  };
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
 
-  player.handleInput(allowedKeys[e.keyCode]);
+    player.handleInput(allowedKeys[e.keyCode]);
 });
